57e2dac6e0b14eef916e49b218f8186a8206c227
JD Patel <jay@citrintech.com>
2020-01-10 00:35:10 +0530

    Touch ID / Face ID

--- LocalAuth/ContentView.swift
+++ LocalAuth/ContentView.swift
@@ -7,11 +7,46 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+    
+    @State private var bgColor = Color.red
+    
     var body: some View {
-        Text("Hello, World!")
+     
+        ZStack {
+            bgColor
+                .edgesIgnoringSafeArea(.all)
+        }
+        .onAppear(perform: authenticate)
+        
     }
+    
+    func authenticate() {
+        
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+         
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+
+                DispatchQueue.main.async {
+                    if success {
+                        self.bgColor = .green
+                    } else {
+                        self.bgColor = .red
+                    }
+                }
+            }
+        } else {
+            self.bgColor = .red
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
