e212b42a7644a0abaa35ea45e21959a1bcc628a7
JD Patel <jay@citrintech.com>
2020-01-10 00:29:02 +0530

    Initial Commit

---
+++ LocalAuth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalAuth
+//
+//  Created by JD Patel on 10/01/20.
+//  Copyright © 2020 JD. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
